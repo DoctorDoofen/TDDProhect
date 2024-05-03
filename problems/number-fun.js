62d95e7160f309d8db1238352dc1e80c50dcc465
DoctorDoofen <and.bennett0930@gmail.com>
2024-05-02 23:28:13 -0500

    phase 2 complete

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,10 +1,21 @@
 function returnsThree() {
-  // Your code here 
+  return 3
 }
 
 function reciprocal(n) {
-  // Your code here 
+  if (n < 1 || n > 1000000) {
+    throw new RangeError('number must be greater than 1 and less than 1000000')
+  }
+  return 1 / n
 }
+
+try {
+  reciprocal(5)
+} catch (error) {
+  console.error('pick a number thats 1 or greater')
+}
+
+
 
 module.exports = {
   returnsThree,
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,29 @@
-// Your code here 
+const { expect } = require('chai')
+
+const { returnsThree, reciprocal } = require('../problems/number-fun');
+
+describe('returnsThree', () => {
+    it('returnsThree function should return the number 3', () => {
+        expect(returnsThree()).to.equal(3)
+    })
+})
+
+describe('reciprocal', () => {
+    context('valid', () => {
+        it('reciprocal function should return the reciprical of the given number', () => {
+            let test01 = reciprocal(5);
+            expect(test01).to.equal(.2)
+        })
+    })
+
+    context('invalid', () => {
+        it('should throw range error for numbers less than 1', () => {
+            let funcCopy = reciprocal.bind(null, 0)
+            expect(funcCopy).to.throw(RangeError)
+        })
+        it('should throw range error for numbers greater than 1,000,000', () => {
+            let funcCopy = reciprocal.bind(null, 1000001)
+            expect(funcCopy).to.throw(RangeError)
+        })
+    })
+})
